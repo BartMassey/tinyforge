e6fe6b3ae749b3df8398671ad7eb024e7eb02acd
Bart Massey <bart@cs.pdx.edu>
2010-04-13 23:14:00 -0700

    added attributions and cleaned up index generation

--- genindices.sh
+++ genindices.sh
@@ -6,6 +6,23 @@ function db {
    sqlite3 -list -separator ' ' bartforge.db "$1"
 }
 
+function bullet {
+   while read NAME VISNAME ATTRIB
+   do
+       if [ "$NAME" = "$VISNAME" ]
+       then
+	   echo -n "  * [[$NAME]]"
+       else
+	   echo -n "  * [[$VISNAME|$NAME]]"
+       fi
+       if [ "$ATTRIB" != "" ]
+       then
+	   echo -n " &mdash; $ATTRIB"
+       fi
+       echo ""
+   done
+}
+
 # generate language index
 ( echo "# Bartforge Index by Programming Language"
   echo ""
@@ -14,32 +31,16 @@ function db {
   do
       echo "$DESC"
       echo ""
-      db "select intname, visname from master where language = '$LANG' order by intname;" |
-      while read NAME VISNAME
-      do
-	  if [ "$NAME" = "$VISNAME" ]
-	  then
-	      echo "  * [[$NAME]]"
-	  else
-	      echo "  * [[$VISNAME|$NAME]]"
-	  fi
-      done
+      db "select intname, visname, attrib from master where language = '$LANG' order by intname;" |
+      bullet
       echo ""
   done ) >language_index.mdwn
 
 # generate alpha index
 ( echo "# Bartforge Alphabetical Index"
   echo ""
-  db "select intname, visname from master order by intname;" |
-  while read NAME VISNAME
-  do
-      if [ "$NAME" = "$VISNAME" ]
-      then
-	  echo "  * [[$NAME]]"
-      else
-	  echo "  * [[$VISNAME|$NAME]]"
-      fi
-  done ) >alpha_index.mdwn
+  db "select intname, visname, attrib from master order by intname;" |
+  bullet ) >alpha_index.mdwn
 
 # generate master index
 ( cat index-top.mdwn
@@ -49,15 +50,7 @@ function db {
   do
       echo "$DESC"
       echo ""
-      db "select intname, visname from master where category = '$CAT' order by intname;" |
-      while read NAME VISNAME
-      do
-	  if [ "$NAME" = "$VISNAME" ]
-	  then
-	      echo "  * [[$NAME]]"
-	  else
-	      echo "  * [[$VISNAME|$NAME]]"
-	  fi
-      done
+      db "select intname, visname, attrib from master where category = '$CAT' order by intname;" |
+      bullet
       echo ""
   done ) >index.mdwn
